51bc64c93f2b9a978bb74d61c30b9b02945f7028
Bartosz Lampart <dzazder@gmail.com>
2019-01-20 15:03:15 +0100

    added result table and some conditions

--- src/components/CurrencyToCurrency.js
+++ src/components/CurrencyToCurrency.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import DICTIONARIES from '../const.js';
 import CurrencyDropdown from './CurrencyDropdown';
 import { Button } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 class CurrencyToCurrency extends Component {
     constructor(props) {
@@ -10,12 +14,14 @@ class CurrencyToCurrency extends Component {
         this.state = {
             baseCurrency: "EUR",
             resultCurrency: "PLN",
-            result: 0.00,
-            error: null
+            result: null,
+            error: null,
+            isCalculating: false
         }
 
         this.checkRate = this.checkRate.bind(this);
         this.handleFieldChange = this.handleFieldChange.bind(this);
+        this.calculateResult = this.calculateResult.bind(this);
     }
 
     handleFieldChange(fieldId, value) {
@@ -23,26 +29,77 @@ class CurrencyToCurrency extends Component {
             case "currFrom": this.setState({ baseCurrency: value }); break;
             case "currTo": this.setState({ resultCurrency: value }); break;
         }
+        this.setState({
+            result: null
+        });
     }    
 
     checkRate() {
+        this.state.isCalculating = true;
+
         var url = "https://api.exchangeratesapi.io/latest?symbols=" + this.state.resultCurrency + "&base=" + this.state.baseCurrency;
         fetch(url)
         .then(res => res.json())
         .then((data) => {
             this.setState({
+                isCalculating: false,
                 result: data.rates[this.state.resultCurrency]   // todo add checking if resultCurrency exists in result data
             });
         },
         (error) => {
             this.setState({
-                error: "Something went wrong"
+                error: "Something went wrong",
+                isCalculating: false
             });
             }
         )
     }
 
+    calculateResult(val) {
+        return Math.round(val * this.state.result * 1000) / 1000;
+    }
+
     render() {
+        var rows = [
+            {"base": 1, "result": this.calculateResult(1)},
+            {"base": 5, "result": this.calculateResult(5)},
+            {"base": 10, "result": this.calculateResult(10)},
+            {"base": 100, "result": this.calculateResult(100)},
+            {"base": 1000, "result": this.calculateResult(1000)}
+        ];
+
+        let resultContainer;
+        if (this.state.isCalculating) {
+            resultContainer = <div>Calculating...</div>
+        }
+        else {
+            resultContainer = 
+            <div>
+                <div>
+                {this.state.result != null &&
+                    <p>
+                        <span>Result: </span>
+                        <span>{this.state.result}</span>
+                    </p>
+                }
+                </div>
+                {this.state.result != null &&
+                    <div>
+                        <Table style={{ width: 400, margin: "0 auto" }}>
+                            <TableBody>
+                            {rows.map(row => (
+                                <TableRow key={row.base}>
+                                    <TableCell align="right">{row.base} {this.state.baseCurrency}</TableCell>
+                                    <TableCell align="left">{row.result} {this.state.resultCurrency}</TableCell>
+                                </TableRow>
+                            ))}
+                            </TableBody>
+                        </Table>
+                    </div>
+                }
+            </div>;
+        }
+
         return(
             <div>
                 <h3>Check the course</h3>
@@ -55,12 +112,7 @@ class CurrencyToCurrency extends Component {
                         Check the course
                     </Button>
                 </div>
-                <div>
-                    <p>
-                        <span>Result: </span>
-                        {this.state.result}
-                    </p>
-                </div>
+                {resultContainer}
             </div>
         );
     }
